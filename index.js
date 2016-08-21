7cded0ac53ad876753225cd5ee049866005c6c92
Paul Ku <justlove0714@hotmail.com>
2016-08-22 02:23:00 +0800

    data get trying to push to firebase for mobile app usage.
    to do: convert string issue. update time.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const requests = require("request");
 const rp = require("request-promise");
 const cheerio  = require("cheerio");
+const firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "./foreclose-c374a2374390.json",
+  databaseURL: "https://foreclose-bdea7.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref();
+var dataRef = ref.child('data/TPD');
 
 
-const baseUrl = "http://www.judicial.gov.tw/";
 const court_code = [
 	{ "code": "TPD", "name": "臺灣台北地方法院" },
 	{ "code": "PCD", "name": "臺灣新北地方法院" },
@@ -28,25 +37,157 @@ const court_code = [
 	{ "code": "LCD", "name": "福建連江地方法院" },
 ]
 
-var options = {
-    uri: baseUrl+"db/WHD2A01.jsp",
-    method:'POST',
-    form: {
-        court: 'TPD'
-    },
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-};
- 
-rp(options)
-    .then(function ($) {
-    	var $form = $("form[name='form']");
-    	console.log($form);
-    },function (err) {
-        console.log("error: ",err);
-    })
-    .then(function(){
+const target_code = [
+	{ "code": "C52","name":"房屋"},
+	{ "code": "C51","name":"土地"},
+	{ "code": "C54","name":"動產"},
+]
 
-    });
-    
+const proc_code = [
+	{ "code": "1","name":"一般程序"},
+	{ "code": "4","name":"應買公告"},
+	{ "code": "5","name":"拍定價格"},
+]
+
+ 
+rp(	{
+	uri: "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A02.jsp?proptype=C52&saletype=1&court=TPD",
+	method:'GET',
+	transform: function (body) { return cheerio.load(body); }
+	})
+	.then(function ($) {
+		var $input = $("input[type='hidden']");
+		return { "name": $input.attr('name'),
+			"value": $input.val()
+		};
+	},function (err) {
+		console.log("error: ",err);
+	})
+	.then(function(result){
+		console.log(result);
+		rp({
+			uri: "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A03.jsp?"+result['name']+"="+result['value']+"&hsimun=all&ctmd=all&sec=all&saledate1=&saledate2=&crmyy=&crmid=&crmno=&dpt=&minprice1=&minprice2=&saleno=&area1=&area2=&registeno=&checkyn=all&emptyyn=all&rrange=%A4%A3%A4%C0&comm_yn=&owner1=&order=odcrm&courtX=TPD&proptypeX=C52&saletypeX=1&query_typeX=db",
+			method: 'GET',
+			transform: function(body){ return cheerio.load(body); }
+		})
+		.then(function($){
+			var rows = $("form[name ='form'] table table tr").slice(1,-2);
+
+			rows.each(function(i,el){
+				var cols = $("td",this);
+				var dataObj = {
+					serial: "", // 字號股別
+					dateNtime: "", // 拍賣日期及拍賣次數
+					cityArea: "", // 縣市
+					address: { link:"", text:"" }, // 房屋地址樓層面積
+					auctionPrice: "", // 總拍賣底價(元)
+					check: "", // 點交
+					isEmpty: "", // 空屋
+					auctionTime: "", // 標別
+					comment: "", // 備註
+					imgLinks: [], // 看圖
+					commAuction: "", // 採通訊投標
+					queryLandPolluted: "", // 土地有無遭受污染
+					strBeauty: function( string ){
+						return string.replace(/\r/g,'').replace(/ /g,'').replace(/\n/g,'').replace(/\t/g,'');
+					}
+				};
+				cols.each(function(i,el){
+					console.log("index: ",i);
+					switch(i) {
+						case 1:
+							dataObj["serial"] = dataObj.strBeauty( $(this).html() );
+							console.log( dataObj["serial"] );
+							break;
+						case 2:
+							dataObj["dateNtime"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 3:
+							dataObj["cityArea"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 4:
+							dataObj["address"]["link"] = "http://aomp.judicial.gov.tw/abbs/wkw/"+$("a",this).attr("href");
+							dataObj["address"]["text"] = dataObj.strBeauty($('a',this).text()) + "|" + dataObj.strBeauty( $(this).text() );
+							console.log( dataObj["address"]["text"] );
+							// dataObj["address"] = dataObj.strBeauty( $(this).html() );
+							
+							break;
+						case 5:
+							dataObj["auctionPrice"] = dataObj.strBeauty( $(this).html() );
+							break;
+						case 6:
+							dataObj["check"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 7:
+							dataObj["isEmpty"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 8:
+							dataObj["auctionTime"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 9:
+							dataObj["comment"] = dataObj.strBeauty( $("div",this).html() );
+							break;
+						case 10:
+							// dataObj["imgLinks"] = dataObj.strBeauty( $(this).html() );
+							// console.log( $(this).html() );
+							break;
+						case 11:
+							dataObj["commAuction"] = dataObj.strBeauty( $("center",this).html() );
+							break;
+						case 12:
+							// dataObj["queryLandPolluted"] = $(this).html();
+							// console.log( $(this).html() );
+							break;
+						default:
+							break;	
+					}
+				}); /* cols each function */
+				delete dataObj["strBeauty"];
+				dataRef.push().set(dataObj);
+			}); /* rows each function */
+			
+			
+
+		}, function(err){
+			console.log("error: ", err);
+		})
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
