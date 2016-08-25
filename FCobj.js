806271493bd35cafff2535990994bbb32c2d3d5d
Paul Ku <justlove0714@hotmail.com>
2016-08-25 14:30:38 +0800

    房屋類別法拍資料存取完畢，土地動產類別待處理

---
+++ FCobj.js
@@ -0,0 +1,206 @@
+const requests = require("request");
+const rp = require("request-promise");
+const iconv = require("iconv-lite");
+const cheerio  = require("cheerio");
+const Q = require('q');
+const firebase = require("firebase");
+const moment = require("moment");
+
+var today = moment().format("YYYY-MM-DD");
+
+firebase.initializeApp({
+  serviceAccount: "./foreclose-c374a2374390.json",
+  databaseURL: "https://foreclose-bdea7.firebaseio.com"
+});
+
+var db = firebase.database();
+var ref = db.ref();
+
+function FCobj(){
+
+	// properties
+	var self = this;
+	var final_page_index_get = false;
+	var final_page_index;
+	var data_array = {};
+
+	this.ArrToObj = function(array){
+		var form_Data = {};
+		for(var key in array){
+			form_Data[ array[key]['name'] ] = array[key]['value'];
+		}
+		return form_Data;
+	}
+
+	this.ObjToArr = function(obj){
+		var arr = [];
+		for(var key in obj){
+			arr.push({
+				name:key,
+				value: obj[key],
+			});
+		}
+		return arr;
+	}
+
+	
+	this.getDataByCourtAndCat = function( target_code,court_code ){
+		var deferred = Q.defer();
+		rp({
+			uri: "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A02.jsp?proptype="+target_code+"&saletype=1&court="+court_code,
+			method: "GET",
+			transform: function(body){
+				return cheerio.load(body);
+			},
+		})
+		.then(function($){
+			var form_Array = $("form[name ='form']").serializeArray();
+			var form_data = self.ArrToObj(form_Array);
+			deferred.resolve(form_data);
+		}, function(error){
+			deferred.reject(error);
+		});
+
+		return deferred.promise;
+	}
+
+	this.strBeauty = function(string){
+		return iconv.decode(string.replace(/\r/g,'').replace(/ /g,'').replace(/\n/g,'').replace(/\t/g,''),'Big5');
+	}
+
+	this.pushData = function($, data){
+		var rowRef = ref.child('data/'+today+'/'+data.courtX);
+		var rows = $("form[name ='form'] table table tr").slice(1,-2);
+
+		rows.each(function(i,el){	
+			var cols = $("td",this);
+			var dataObj = {
+				serial: "", // 字號股別
+				dateNtime: "", // 拍賣日期及拍賣次數
+				cityArea: "", // 縣市
+				address: { link:"", text:"" }, // 房屋地址樓層面積
+				auctionPrice: "", // 總拍賣底價(元)
+				check: "", // 點交
+				isEmpty: "", // 空屋
+				auctionTime: "", // 標別
+				comment: "", // 備註
+				imgLinks: [], // 看圖
+				commAuction: "", // 採通訊投標
+				queryLandPolluted: "" // 土地有無遭受污染
+			};
+			cols.each(function(i,el){
+				switch(i) {
+					case 1:
+						dataObj["serial"] = self.strBeauty( $(this).html() );
+						break;
+					case 2:
+						dataObj["dateNtime"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 3:
+						dataObj["cityArea"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 4:
+						dataObj["address"]["link"] = "http://aomp.judicial.gov.tw/abbs/wkw/"+$("a",this).attr("href");
+						dataObj["address"]["text"] = self.strBeauty($('a',this).text()) + "|" + self.strBeauty( $(this).text() );							
+						break;
+					case 5:
+						dataObj["auctionPrice"] = self.strBeauty( $(this).html() );
+						break;
+					case 6:
+						dataObj["check"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 7:
+						dataObj["isEmpty"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 8:
+						dataObj["auctionTime"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 9:
+						dataObj["comment"] = self.strBeauty( $("div",this).html() );
+						break;
+					case 10:
+						// dataObj["imgLinks"] = dataObj.strBeauty( $(this).html() );
+						// console.log( $(this).html() );
+						break;
+					case 11:
+						dataObj["commAuction"] = self.strBeauty( $("center",this).html() );
+						break;
+					case 12:
+						// dataObj["queryLandPolluted"] = $(this).html();
+						// console.log( $(this).html() );
+						break;
+					default:
+						break;	
+				}
+				
+			});	// cols each function 
+			rowRef.push().set(dataObj);
+		});	// rows each function
+	}
+
+
+	this.getPageData = function(formObj, pageNow){
+		var deferred = Q.defer();
+		var form = self.ObjToArr(formObj);
+		var url = "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A03.jsp?"+form[0]['name']+"="+form[0]['value']+"&hsimun=all&ctmd=all&sec=all&saledate1=&saledate2=&crmyy=&crmid=&crmno=&dpt=&minprice1=&minprice2=&saleno=&area1=&area2=&registeno=&checkyn=all&emptyyn=all&rrange=%A4%A3%A4%C0&comm_yn=&owner1=&order=odcrm&courtX="+formObj.courtX+"&proptypeX="+formObj.proptypeX+"&saletypeX="+formObj.saletypeX+"&query_typeX=db";
+		rp({
+			uri: url,
+			method: 'POST',
+			form: formObj,
+			transform: function(body){ return cheerio.load(body); }
+		})
+		.then(function($){
+
+			var form_Array = $("form[name ='form']").serializeArray();
+			var form_Data = {};
+			form_Data = self.ArrToObj(form_Array);
+
+			if( $("nobr").length == 0 ){
+				// 沒有資料提早結束
+				deferred.resolve({
+					form: {},
+					pageNow: 1,
+					final: true,
+				});
+			} else {
+				self.pushData($, form_Data);
+
+				if( !this.final_page_index_get ){
+					this.final_page_index = ($("nobr").length == 1) ? 1 : parseInt( $("nobr").last().attr("onclick").split("=")[1].split(";")[0] );
+					this.final_page_index_get = true;
+					console.log("最後一頁: ", this.final_page_index);
+				}
+
+				var final_bool = ( form_Data.pageNow == this.final_page_index ) ? true : false;
+				form_Data.pageNow = ( form_Data.pageNow == undefined ) ? 1 : parseInt(form_Data.pageNow) + 1;
+
+				if( final_bool ){
+					this.final_page_index_get = false;
+				}
+
+				deferred.resolve({
+					form: form_Data,
+					pageNow: form_Data.pageNow,
+					final: final_bool,
+				});	
+			}
+			
+			
+			
+		}, function(err){
+			deferred.reject(err);
+			
+		}); 
+
+		return deferred.promise;
+	}
+
+	this.reset = function(){
+		var deferred = Q.defer();
+		this.final_page_index = undefined;
+		deferred.resolve();
+		return deferred.promise;
+	}
+}
+
+exports = module.exports = new FCobj;
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const requests = require("request");
-const rp = require("request-promise");
-const cheerio  = require("cheerio");
-const firebase = require("firebase");
-
-firebase.initializeApp({
-  serviceAccount: "./foreclose-c374a2374390.json",
-  databaseURL: "https://foreclose-bdea7.firebaseio.com"
-});
-
-var db = firebase.database();
-var ref = db.ref();
-var dataRef = ref.child('data/TPD');
+const FCobj = require("./FCobj");
 
 
+
+// court
 const court_code = [
 	{ "code": "TPD", "name": "臺灣台北地方法院" },
 	{ "code": "PCD", "name": "臺灣新北地方法院" },
 	{ "code": "SLD", "name": "臺灣士林地方法院" },
-	{ "code": "TYP", "name": "臺灣桃園地方法院" },
+	{ "code": "TYD", "name": "臺灣桃園地方法院" },
 	{ "code": "SCD", "name": "臺灣新竹地方法院" },
 	{ "code": "MLD", "name": "臺灣苗栗地方法院" },
 	{ "code": "TCD", "name": "臺灣臺中地方法院" },
@@ -37,121 +27,66 @@ const court_code = [
 	{ "code": "LCD", "name": "福建連江地方法院" },
 ]
 
+// proptype
 const target_code = [
 	{ "code": "C52","name":"房屋"},
 	{ "code": "C51","name":"土地"},
 	{ "code": "C54","name":"動產"},
 ]
 
+// saletype
 const proc_code = [
 	{ "code": "1","name":"一般程序"},
 	{ "code": "4","name":"應買公告"},
 	{ "code": "5","name":"拍定價格"},
 ]
+	
 
- 
-rp(	{
-	uri: "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A02.jsp?proptype=C52&saletype=1&court=TPD",
-	method:'GET',
-	transform: function (body) { return cheerio.load(body); }
-	})
-	.then(function ($) {
-		var $input = $("input[type='hidden']");
-		return { "name": $input.attr('name'),
-			"value": $input.val()
-		};
-	},function (err) {
-		console.log("error: ",err);
-	})
-	.then(function(result){
-		console.log(result);
-		rp({
-			uri: "http://aomp.judicial.gov.tw/abbs/wkw/WHD2A03.jsp?"+result['name']+"="+result['value']+"&hsimun=all&ctmd=all&sec=all&saledate1=&saledate2=&crmyy=&crmid=&crmno=&dpt=&minprice1=&minprice2=&saleno=&area1=&area2=&registeno=&checkyn=all&emptyyn=all&rrange=%A4%A3%A4%C0&comm_yn=&owner1=&order=odcrm&courtX=TPD&proptypeX=C52&saletypeX=1&query_typeX=db",
-			method: 'GET',
-			transform: function(body){ return cheerio.load(body); }
-		})
-		.then(function($){
-			var rows = $("form[name ='form'] table table tr").slice(1,-2);
 
-			rows.each(function(i,el){
-				var cols = $("td",this);
-				var dataObj = {
-					serial: "", // 字號股別
-					dateNtime: "", // 拍賣日期及拍賣次數
-					cityArea: "", // 縣市
-					address: { link:"", text:"" }, // 房屋地址樓層面積
-					auctionPrice: "", // 總拍賣底價(元)
-					check: "", // 點交
-					isEmpty: "", // 空屋
-					auctionTime: "", // 標別
-					comment: "", // 備註
-					imgLinks: [], // 看圖
-					commAuction: "", // 採通訊投標
-					queryLandPolluted: "", // 土地有無遭受污染
-					strBeauty: function( string ){
-						return string.replace(/\r/g,'').replace(/ /g,'').replace(/\n/g,'').replace(/\t/g,'');
-					}
-				};
-				cols.each(function(i,el){
-					console.log("index: ",i);
-					switch(i) {
-						case 1:
-							dataObj["serial"] = dataObj.strBeauty( $(this).html() );
-							console.log( dataObj["serial"] );
-							break;
-						case 2:
-							dataObj["dateNtime"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 3:
-							dataObj["cityArea"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 4:
-							dataObj["address"]["link"] = "http://aomp.judicial.gov.tw/abbs/wkw/"+$("a",this).attr("href");
-							dataObj["address"]["text"] = dataObj.strBeauty($('a',this).text()) + "|" + dataObj.strBeauty( $(this).text() );
-							console.log( dataObj["address"]["text"] );
-							// dataObj["address"] = dataObj.strBeauty( $(this).html() );
-							
-							break;
-						case 5:
-							dataObj["auctionPrice"] = dataObj.strBeauty( $(this).html() );
-							break;
-						case 6:
-							dataObj["check"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 7:
-							dataObj["isEmpty"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 8:
-							dataObj["auctionTime"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 9:
-							dataObj["comment"] = dataObj.strBeauty( $("div",this).html() );
-							break;
-						case 10:
-							// dataObj["imgLinks"] = dataObj.strBeauty( $(this).html() );
-							// console.log( $(this).html() );
-							break;
-						case 11:
-							dataObj["commAuction"] = dataObj.strBeauty( $("center",this).html() );
-							break;
-						case 12:
-							// dataObj["queryLandPolluted"] = $(this).html();
-							// console.log( $(this).html() );
-							break;
-						default:
-							break;	
-					}
-				}); /* cols each function */
-				delete dataObj["strBeauty"];
-				dataRef.push().set(dataObj);
-			}); /* rows each function */
-			
-			
+var court_index = 0;
 
-		}, function(err){
-			console.log("error: ", err);
-		})
-	});
+function StartSingleCourtScratch(i){
+	console.log(court_code[i]["name"]+", 類型: "+ target_code[0]["name"] +": 資料抓取中...");
+	FCobj
+		.getDataByCourtAndCat( target_code[0]["code"], court_code[i]["code"] )
+		.then(function(result){
+			recursivePageSave(result, result.pageNow);
+		},function(error){
+			console.error(error);
+		});
+		
+		
+}
+
+function recursivePageSave(result, pageNow){
+	FCobj
+		.getPageData(result,pageNow)
+		.then(function(result){
+			if( result.final ){
+				console.log( "page "+pageNow+" is saved. " );
+				console.log( "Data save complete." );
+				delete FCobj.final_page_index;
+				court_index++;
+				if( !checkIfFinished(court_index) ){
+					StartSingleCourtScratch(court_index);
+				}
+				return;
+			} else{
+				console.log( "page "+pageNow+" is saved. " );
+				recursivePageSave( result.form, result.pageNow );
+			}
+		}, function(error){
+			console.error(error);
+		});
+}
+
+function checkIfFinished(i){
+	return (i == court_code.length  ) ? true : false;
+}
+
+// program start...
+StartSingleCourtScratch(0);
+
 
 
 
